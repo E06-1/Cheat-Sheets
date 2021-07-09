d4c8cab57c9e3b09771f9adc81329f45ec5596df
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-07-09 21:40:13 +0200

    Add comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+//Site Object, will be a virtual Model of our Site to work with in JS
 const site = {
   root: null,
   sheets: null,
 
+  /*Method will initialize the site object by setting the root folder and fetching sheet names*/
   init: async function () {
+
+    /*Live-Server starts index.html directly while github-pages starts the root, 
+    removing index from the end will work on both live-server and github pages*/
     if (window.location.href.endsWith("index.html")) {
       window.location.href = window.location.href.replace(/index.html$/, "");
     } else {
@@ -17,12 +22,18 @@ const site = {
     this.loadSheet(this.sheets[Math.floor(Math.random() * this.sheets.length)]);
   },
 
-  /*TODO: Dont use Element.innerHTML */
+  /*Method will display a single Cheat-Sheet 
+  as specified by it's Filename (no Path necessary) 
+  inside the HTML <main></main> element*/
   loadSheet: async function (sheetName) {
     const main = document.getElementsByTagName("main")[0];
+    /*TODO: Dont use Element.innerHTML */
     main.innerHTML = await this.getFileFromServer(`/sheets/${sheetName}`);
   },
 
+  /*Method will get the specified File from the Server
+  currently File names need to be absolute paths.
+  For example "/sheets/example.html" Return value is a promise. */
   getFileFromServer: async function (path) {
     /*TODO: Implement relative Paths like "./file" or "../folder/file" */
     /*TODO: Status Code handling*/
@@ -45,4 +56,5 @@ const site = {
   },
 };
 
+//Initialize Site Object on page load
 window.addEventListener("onload", site.init());
